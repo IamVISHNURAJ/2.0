d61f6cf0ab68c4b8335c6b8558406bfa67e1ca58
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-21 08:49:39 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+int main ()
+{
+  int n;
+  scanf ("%d", &n);
+  if(n>0){
+  if (n % 2 == 0)
+    printf ("Even");
+  else
+  printf("Odd");
+ }
+ else
+printf ("invaild");
+  return 0;
+}
